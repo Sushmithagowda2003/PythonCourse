b38057288be5dc6abd570e806cba644c998d44d9
Sushmithagowda2003 <122030217+Sushmithagowda2003@users.noreply.github.com>
2024-09-05 10:14:26 +0000

    second

---
+++ day3.py
@@ -0,0 +1,13 @@
+print("welcome to treasure Island")
+print("your mission is to find the treasure")
+choice1=input("you\ are at crossroad ,where do you want to go? "Left" or "Right":)
+if chooice1=="Left":
+    #continue the game
+else "Right":
+    print("your game over")
+choice2=input("type "wait" to wait and "swim" to swim:)
+
+
+
+
+
